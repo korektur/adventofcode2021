99a24ddf456a4ac79651352f17029ae43f76d3b3
Ruslan Akhundov <akhundov2@gmail.com>
2021-12-17 21:19:17 +0000

    day17

---
+++ src/main/kotlin/Day17.kts
@@ -0,0 +1,38 @@
+import java.io.File
+import kotlin.math.max
+
+val input = File("src/main/resources/day17.in").readLines()[0]
+    .removePrefix("target area: ")
+    .split(", y=", "..", "x=")
+
+val minX = input[1].toInt()
+val maxX = input[2].toInt()
+val minY = input[3].toInt()
+val maxY = input[4].toInt()
+
+var ans = Int.MIN_VALUE
+var ans2 = 0
+val ansSet = mutableSetOf<Pair<Int, Int>>()
+for (xV in 1..maxX) {
+    for (yV in minY..5000) {
+        var curX = 0
+        var curY = 0
+        var step = 0
+        var curAns = 0
+        while (curX <= maxX && curY >= minY) {
+            curX += max(xV - step, 0)
+            curY += yV - step
+            step++
+            curAns = max(curAns, curY)
+            if (curX in minX..maxX && curY in minY..maxY) {
+                ans = max(curAns, ans)
+                ans2++
+                ansSet.add(xV to yV)
+                break
+            }
+        }
+    }
+}
+
+println(ans)
+println(ans2)
